42ced8943e5d0513fb7d98fd2bf339724c5912d9
Jess Gardner <inanna1130@gmail.com>
2017-07-19 17:42:37 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -1,10 +1,18 @@
 var express = require('express');
-var hbs = require('hbs');
 
 var app = express();
 
+app.get('/', (req, res) => {
+    res.send(`Welcome to Pizza Express!`);
+});
 
+app.get('/topping/:type', (req, res) => {
+    res.send(`${req.params.type} pizza! Good choice.`);
+});
 
+app.get('/order/:amount/:size', (req, res) => {
+    res.send(`Your order for ${req.params.amount} ${req.params.size} pizzas will be ready in 1 minute!`);
+});
 
 
 
