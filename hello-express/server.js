87f5a050a7a5763a230c85cf5db6e81a8454a971
Jess Gardner <inanna1130@gmail.com>
2017-07-19 17:18:06 -0400

    Added labs.

--- hello-express/server.js
+++ hello-express/server.js
@@ -1,76 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
-    // res.render('index.html');    what sends the html to client
-    // res.redirect('/greeting');   sends to greeting from index page
-    console.log('req.route');
-    console.log('req.query');
-    console.log('req.params');
-    // console.log('res');
+app.get('/greeting', (req, res) => {
+    res.render('greeting', {
+        saying: req.query.saying,
+        name: req.query.name
+    });
 });
 
-app.get('/greetings', (req, res) => {
-    res.send('Hey WDI 11!');
+app.get('/favorite-foods', function(req, res) {
+    var favoriteFoods = ["Jeni's almond butter brittle ice cream", 'Tacos from Superica', 'a breakfast sandwich from Gjelina to go in Venice', 'Croissants from Bottega Louie in downtown Los Angeles', 'Pizza from Little Star in San Francisco'];
+    
+    res.render('favorite-foods', {
+        data: favoriteFoods
+    });
 });
-
-app.get('/rihanna', (req, res) => {
-    res.send('work work work work work');
-});
-
-app.get('/users/:user_id/friends/:friend_id', (req, res) =>{
-    console.log(req.query);
-    res.send(`Hello user #:${req.params.user_id}. Your friend is user #:${req.params.friend_id}.`)
-});
-
-app.get('/food/:food', (req, res) =>{
-    res.send(`I really really love ${req.params.food}.`)
-}) ;
-
-app.get('/sightings', (req, res) => {
-    console.log(req.query);
-    res.send(`How many ufo sightings do you think there are in ${req.query.state}? ${req.query.sights}.`);
-});
-
-// app.get('/math/add', (req, res) => {
-// const value = parseInt(req.query.num1) + parseInt(req.query.num2);
-//     res.send(`${req.query.num1} + ${req.query.num2} = ${value}`);
-// });
-
-// app.get('/math/subtract', (req, res) => {
-// const value = parseInt(req.query.num1) - parseInt(req.query.num2);
-//     res.send(`${req.query.num1} - ${req.query.num2} = ${value}`);
-// });
-
-// app.get('/math/multiply', (req, res) => {
-// const value = parseInt(req.query.num1) * parseInt(req.query.num2);
-//     res.send(`${req.query.num1} * ${req.query.num2} = ${value}`);
-// });
-
-// app.get('/math/divide', (req, res) => {
-// const value = parseInt(req.query.num1) / parseInt(req.query.num2);
-//     res.send(`${req.query.num1} / ${req.query.num2} = ${value}`);
-// });
-
-app.get('/math/:operator', (req, res) => {
-    if (req.params.operator === '+') {
-        var value = parseInt(req.query.num1) + parseInt(req.query.num2);
-        res.send(`${req.query.num1} + ${req.query.num2} = ${value}`);
-    } else if (req.params.operator === 'subtract') {
-        var value = parseInt(req.query.num1) - parseInt(req.query.num2);
-        res.send(`${req.query.num1} - ${req.query.num2} = ${value}`);
-    } else if (req.params.operator === '*') {
-        var value = parseInt(req.query.num1) * parseInt(req.query.num2);        
-        res.send(`${req.query.num1} * ${req.query.num2} = ${value}`);
-    } else if (req.params.operator === 'divide') {
-        var value = parseInt(req.query.num1) / parseInt(req.query.num2);
-        res.send(`${req.query.num1} / ${req.query.num2} = ${value}`);
-    }
-});
-
-
 
 
 
@@ -81,5 +29,5 @@ app.get('/math/:operator', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("hello-express is running on port " + port);  
+    console.log(`Server is running on port ${port}`);  
 });
