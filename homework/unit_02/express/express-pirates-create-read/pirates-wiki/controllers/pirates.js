5b94269d8397f58709412a3c205c3d36a102c31a
Jess Gardner <inanna1130@gmail.com>
2017-07-20 18:01:06 -0400

    Set up server and MVC

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,18 @@
-//your code below
+// REQUIRES
+const express = require('express');
+const router = express.Router();
+const pirates = require('../models/pirates.js');
+
+
+
+
+
+
+
+
+
+
+
+
+// EXPORTS
+module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,32 @@
-//your code here
+// APP REQUIRES
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+// APP SETUP
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// VIEWS
+app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+
+
+
+
+//controllers for `/pirates` resource
+var pirateController = require('./controllers/pirates.js');
+app.use('/pirates', pirateController);
+
+
+
+// SERVER 
+app.listen(port, (req, res) => {
+    console.log(`server running on ${port}`);
+});
