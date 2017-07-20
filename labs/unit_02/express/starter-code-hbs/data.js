fd782616e548bdd1124711106f0b96ebf3e78bf2
Jess Gardner <inanna1130@gmail.com>
2017-07-20 17:59:34 -0400

    Class exercises

--- labs/unit_02/express/starter-code-hbs/data.js
+++ labs/unit_02/express/starter-code-hbs/data.js
@@ -1,1 +1,22 @@
-module.exports = { };
+module.exports = {
+
+  seededTodos: [
+    {
+      description: "get beer",
+      urgent: true
+    }, {
+      description: "dry cleaning",
+      urgent: false
+    }, {
+      description: "feed fish",
+      urgent: true
+    }, {
+      description: "get a job",
+      urgent: true
+    }, {
+      description: "get all the money",
+      urgent: true
+    }
+  ]
+
+ };
