5d6ce0422c7996c2df65e9869382dd99ff317c3e
Jess Gardner <inanna1130@gmail.com>
2017-07-25 00:50:06 -0400

    part 5 added start and stop

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -9,21 +9,38 @@ class Tamagotchi {
     this.foodInTummy = 10;
     this.restedness = 10;
     this.health = 10;
-  }
+  };
   cry(){
     // this.foodInTummy -= 1;
     // console.log('Current food in tummy: ' + this.foodInTummy);
     console.log(this.name);
     console.log('WAHH!!');
-  }
+  };
   puke(){
     this.foodInTummy--;
     console.log(`Only ${this.foodInTummy} foods in tummy! WAHH!`);
-  }
+  };
   yawn(){
     this.restedness--;
     console.log(`${this.name} has a current restedness level of: ${this.restedness}`);
-  }
+  };
+  start(){
+    console.log(this.name);
+    this.hungerTimer = setInterval(function(){
+        this.cry();
+    }, 6000);
+    this.yawnTimer = setInterval(function(){
+        this.yawn();
+    }, 10000);
+    this.sickTimer = setInterval(function(){
+        this.puke();
+    }, 20000);
+  };
+  stop(){
+    clearInterval(this.hungerTimer);
+    clearInterval(this.yawnTimer);
+    clearInterval(this.sickTimer);
+  };
 }
 
 
