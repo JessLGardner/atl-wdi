4572c961b3038472a9f90eddf16534ec0161b444
Jess Gardner <inanna1130@gmail.com>
2017-07-26 21:25:32 -0400

    created donut schema

--- homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,15 @@
 //requirements: require mongoose
-
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 //create your donut schema:
-
+const DonutSchema = new Schema({
+    name: String,
+    description: String,
+    image: String,
+    price: Number,
+    quantity: Number
+});
 
 //export your donut with module.exports()
+module.exports = DonutSchema;
