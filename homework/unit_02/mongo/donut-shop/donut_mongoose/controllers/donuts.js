fa679ee4c4eaa21509c4422e7fd92e57b05663ff
Jess Gardner <inanna1130@gmail.com>
2017-07-26 23:37:13 -0400

    Index route created

--- homework/unit_02/mongo/donut-shop/donut_mongoose/controllers/donuts.js
+++ homework/unit_02/mongo/donut-shop/donut_mongoose/controllers/donuts.js
@@ -2,15 +2,34 @@
 // REQUIREMENTS
 //======================
 // require express, router, mongoose, Donut schema
-var express = require("express");
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const DonutModel = require("../models/donuts");
 
 //======================
 // INDEX
 //======================
 // Create a GET index route "/" that sends all donuts to index.hbs
-
+router.get('/', (req, res)=>{
+    DonutModel.find({})
+        .then((donut)=>{
+            console.log('calling index page');
+            // res.send(donut);
+            res.render('donuts/index', 
+                {
+                donut,
+                // donutd: donut._id,
+                // donutImg: donut.img,
+                // donutName: donut.name,
+                // donutPrice: donut.price
+                }
+            )
+        })
+        .catch((err)=>{
+            console.log('Error finding donuts!');
+            console.log(err);
+        })
+});
 
 
 //======================
@@ -24,7 +43,9 @@ var router = express.Router();
 // SHOW
 //======================
 // Create a GET show route "/:id" that renders the donut's show page
+// router.get('/:id', (req, res)=>{
 
+// })
 
 
 
@@ -65,3 +86,16 @@ var router = express.Router();
 //======================
 // export router with module.exports
 module.exports = router;
+
+
+
+
+// Set up your requirements. DON'T FORGET to module.exports the router at the bottom.
+// Create a GET index route "/" that sends all donuts to index.hbs
+// Create a GET show route "/:id" that renders the donut's show page
+// Create a GET new route "/new" that renders the new.hbs form
+// Create a POST index route "/" that creates a new donut and upon success redirects back to the index page
+// Create a GET edit route "/:id/edit" that renders the edit.hbs page and sends that donut's data to it
+// Create a PUT update route "/:id" that updates the donut and redirects back to the SHOW PAGE (not index)
+// Create a DELETE delete route "/:id" that deletes the donut and redirects back to index page "/"
+// 🎯 Git add, git commit after each route
