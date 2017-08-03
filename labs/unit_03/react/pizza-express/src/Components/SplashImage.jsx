880d9b179157163a6fa49a63f50774065562e01e
Jess Gardner <inanna1130@gmail.com>
2017-08-03 14:42:43 -0400

    Fixed SI.jsx

--- labs/unit_03/react/pizza-express/src/Components/SplashImage.jsx
+++ labs/unit_03/react/pizza-express/src/Components/SplashImage.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import SplashPizza from '../images/splashpizza.jpg';
 
 class SplashImage extends Component {
     render(){
         return(
             <div className="splash-image">
-                <img src={SplashPizza} alt=""/>
+                <div>
+                    <span>FIGHT FOR THE LAST SLICE.</span>
+                </div>
             </div>
         );
     }
