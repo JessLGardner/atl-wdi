ae3a401f8b95a5dd5b6bf5d99382be63f88f8c5a
Jess Gardner <inanna1130@gmail.com>
2017-08-03 14:56:59 -0400

    Created Description.jsx

--- labs/unit_03/react/pizza-express/src/App.js
+++ labs/unit_03/react/pizza-express/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import SplashImage from './Components/SplashImage';
+import Description from './Components/Description';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SplashImage/>
+      <div>
+        <div className="App">
+          <SplashImage/>
+        </div>
+        <div>
+          <Description/>
+        </div>
+
+
+
+
       </div>
     );
   }
--- labs/unit_03/react/pizza-express/src/Components/Description.jsx
+++ labs/unit_03/react/pizza-express/src/Components/Description.jsx
@@ -1,2 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import DescripImg from '../images/pizzaoven.jpg';
+
+
+class Description extends Component {
+    render(){
+        return (
+            <div className="description">
+                <img className="description img" src={DescripImg} alt=""/>
+                <p className="description p" >Andouille beef chicken chuck cow drumstick fatback filet ham hock kielbasa loin meatball meatloaf mignon pancetta pastrami pig prosciutto sirloin spare strip swine tenderloin turducken venison. Bresaola capicola chuck corned drumstick kielbasa leberkas loin meatloaf pancetta pork shank shoulder spare strip tenderloin tongue turducken. Bacon beef belly capicola chicken frankfurter ham kielbasa leberkas meatloaf mignon pancetta pastrami ribeye rump salami shank steak tongue turkey. Andouille beef biltong bresaola chicken chop drumstick fatback filet flank kielbasa loin round salami sausage shank sirloin strip t-bone tenderloin tongue tri-tip. Beef brisket drumstick fatback filet frankfurter hock jerky kielbasa pastrami ribs round shankle swine tail tongue turducken.</p>
+            </div>
+        );
+    }
+}
+
+export default Description;
 
