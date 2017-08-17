e667e120f20c37d76e46750948213ba44dedc830
Jess Gardner <inanna1130@gmail.com>
2017-08-17 18:28:21 -0400

    NFL - queried some NFL stuff

---
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -0,0 +1,5 @@
+1. SELECT name FROM teams;
+2. SELECT stadium, head_coach FROM teams WHERE UPPER(conference) = UPPER('NFC');
+3. SELECT head_coach FROM teams WHERE UPPER(conference) = UPPER('AFC') AND UPPER(division) = UPPER('South');
+4. SELECT COUNT (*) FROM players;
+5.
