1d5efe294b5378175aee197ff9e22d48f21737b3
Jess Gardner <inanna1130@gmail.com>
2017-08-21 16:51:07 -0400

    3. Integers Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -5,6 +5,7 @@ b = (2 + 2 == 3)
 puts a == b
 puts a != b
 
+
 # 2.
 cats = nil
 puts cats
@@ -13,4 +14,12 @@ print cats
 puts cats.class
 
 
+# 3.
+d = 10
+puts d
 
+e = 10.5
+puts e
+
+d = e   
+puts d
