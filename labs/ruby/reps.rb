23384d5da9a8327881db3f3efc884d13fb924ffe
Jess Gardner <inanna1130@gmail.com>
2017-08-21 16:38:47 -0400

    ruby labs

---
+++ labs/ruby/reps.rb
@@ -0,0 +1,32 @@
+# words = ["hello", "what", "is", "up", "dude"]
+
+# word_length = words.map do |word|
+#     word.length
+# end
+# puts word_length
+
+
+# def fizzbuzz(i)
+#         if i % 3 == 0 && i % 5 == 0
+#             puts "Fizzbuzz"
+#         elsif i % 3 != 0 && i % 5 != 0
+#             puts "Sorrels"
+#         elsif i % 3 == 0 
+#             puts "Fizz"
+#         elsif i % 5 == 0
+#             puts "Buzz"
+#         else 
+#             puts i
+#         end
+# end
+
+# fizzbuzz(3)
+# fizzbuzz(5)
+# fizzbuzz(15)
+# fizzbuzz(19)
+
+array_to_test = [ ["Husker","Adama"], ["DrRobotmck"], [] ]
+
+def hasher
+
+end
